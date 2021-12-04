b1256a45ebfc4e9d3bac768fb04f275fa6eb048b
John Clary <john.clary@austintexas.gov>
2021-12-04 14:19:55 -0600

    add is_active to sessions

---
+++ migrations/default/1638595823459_alter_table_public_sessions_add_column_is_active/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."sessions" add column "is_active" boolean
+--  not null default 'true';
